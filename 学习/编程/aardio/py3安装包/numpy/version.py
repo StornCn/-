7f6995083ca6f996ce2a0a8ec8bdb03b1ff28518
Home <642226127@qq.com>
2020-02-09 20:50:46 +0800

    添加aardio_py3库

---
+++ 学习/编程/aardio/py3安装包/numpy/version.py
@@ -0,0 +1,12 @@
+
+# THIS FILE IS GENERATED FROM NUMPY SETUP.PY
+#
+# To compare versions robustly, use `numpy.lib.NumpyVersion`
+short_version = '1.16.6'
+version = '1.16.6'
+full_version = '1.16.6'
+git_revision = 'd94bd14612775b3e48c3b13e671984f35ef98dda'
+release = True
+
+if not release:
+    version = full_version
